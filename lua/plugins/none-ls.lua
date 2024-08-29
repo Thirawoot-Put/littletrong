23613418f60c7687f3f79323aea8167eb197a1d7
Thirawoot.P <152477483+Thirawoot-Put@users.noreply.github.com>
2024-08-29 11:34:30 +0700

    none-ls-extra

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
@@ -10,8 +13,9 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.rubocop,
+
+        require("none-ls.diagnostics.eslint_d")
       },
     })
 
