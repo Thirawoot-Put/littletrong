e89ebb2079b68f6b04af50de32bd2569cd2f02ec
Thirawoot.P <152477483+Thirawoot-Put@users.noreply.github.com>
2024-08-30 14:30:57 +0700

    tssserver config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,40 +1,57 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
+    cmd = "LspInfo",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason-lspconfig.nvim" },
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabiliites = capabilities,
-			})
-			lspconfig.html.setup({
-				capabiliites = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabiliites = capabilities,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabiliites = capabilities,
+      })
+      lspconfig.html.setup({
+        capabiliites = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabiliites = capabilities,
+        root_dir = lspconfig.util.root_pattern(
+          '.eslintrc',
+          '.eslintrc.js',
+          '.eslintrc.cjs',
+          '.eslintrc.yaml',
+          '.eslintrc.yml',
+          '.eslintrc.json'
+        -- Disabled to prevent "No ESLint configuration found" exceptions
+        -- 'package.json',
+        ),
+      })
       lspconfig.prismals.setup({
         capabilities = capabilities
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
