3643ea9b2a93f06c28596468a0f37c9d9f988b3c
Thirawoot.P <152477483+Thirawoot-Put@users.noreply.github.com>
2024-09-01 07:08:09 +0700

    greeter alpha

---
+++ lua/littletrong/plugins/alpha.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'goolord/alpha-nvim',
+    event = 'VimEnter',
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      dashboard.section.header.val = {
+          [[ ████████  ██████   ██████   ████   ████  ]],
+          [[    ██     ██   ██ ██    ██ ██  ██ ██  ██ ]],
+          [[    ██     ██████  ██    ██ ██  ██ ██  ██ ]],
+          [[    ██     ██   ██ ██    ██ ██  ██ ██  ██ ]],
+          [[    ██     ██   ██  ██████   ████   ████  ]],
+      }
+
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  > New file", "<cmd>ene <CR>"),
+        dashboard.button("SPC ee", "  > Toggle file explorer", ":NvimTreeToggle <CR>"),
+        dashboard.button("SPC ff", "  > Find file", ":Telescope find_files <CR>"),
+        dashboard.button("SPC fs", "  > Find text", ":Telescope live_grep <CR>"),
+      } 
+
+      alpha.setup(dashboard.opts)
+
+      vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end
+  },
+}
