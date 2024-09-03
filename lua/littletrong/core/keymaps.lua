9309ea1e1ab452e0700925f18b1cb16fc76896c0
Thirawoot.P <152477483+Thirawoot-Put@users.noreply.github.com>
2024-09-03 13:41:05 +0700

    keymap for split & remove

--- lua/littletrong/core/keymaps.lua
+++ lua/littletrong/core/keymaps.lua
@@ -2,12 +2,17 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", {desc = "Exit insert mode with jk"})
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "Clear search hightlights"})
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search hightlights" })
 
 
 keymap.set("n", "<c-k>", ":wincmd k<CR>")
 keymap.set("n", "<c-j>", ":wincmd j<CR>")
 keymap.set("n", "<c-l>", ":wincmd l<CR>")
 keymap.set("n", "<c-h>", ":wincmd h<CR>")
+
+keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' })
+keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontall' })
+keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
+keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
