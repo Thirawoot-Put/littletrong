05e7d0f00551fe600c4fb070dc62ee4d0cb4ded2
Thirawoot.P <thirawoot.put@gmail.com>
2024-10-24 10:08:37 +0700

    fixed signature and hover conflict (disable in noice)

--- lua/littletrong/plugins/noice.lua
+++ lua/littletrong/plugins/noice.lua
@@ -16,6 +16,8 @@ return {
     local noice = require("noice")
     noice.setup({
       lsp = {
+        hover = { enabled = false }, -- Disable hover if needed
+        signature = { enabled = false },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -25,11 +27,11 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true,     -- use a classic bottom cmdline for search
-        command_palette = true,   -- position the cmdline and popupmenu together
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,   -- add a border to hover docs and signature help
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
     })
   end
