736e04c1ae53b68d4b18dad09c88e6a876decb44
Thirawoot.P <thirawoot.put@gmail.com>
2024-10-29 09:59:40 +0700

    change notify

--- lua/littletrong/plugins/noice.lua
+++ lua/littletrong/plugins/noice.lua
@@ -10,11 +10,20 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    -- "rcarriga/nvim-notify",
   },
   config = function()
     local noice = require("noice")
     noice.setup({
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          }
+        },
+      },
       lsp = {
         hover = { enabled = false }, -- Disable hover if needed
         signature = { enabled = false },
