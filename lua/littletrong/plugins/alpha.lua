93b2ae23d7ced1fe624dd22dc358cc58fc342a0b
Thirawoot.P <thirawoot.put@gmail.com>
2024-10-31 10:18:47 +0700

    change dashboard ascii art

--- lua/littletrong/plugins/alpha.lua
+++ lua/littletrong/plugins/alpha.lua
@@ -7,15 +7,15 @@ return {
       local dashboard = require("alpha.themes.dashboard")
 
       dashboard.section.header.val = {
-        "                                                     ",
-        "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-        "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-        "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-        "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-        "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-        "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-        "                                                     ",
 
+        "                                                          ",
+        "   _/_/_/_/_/  _/_/_/      _/_/    _/      _/    _/_/_/   ",
+        "      _/      _/    _/  _/    _/  _/_/    _/  _/          ",
+        "     _/      _/_/_/    _/    _/  _/  _/  _/  _/  _/_/     ",
+        "    _/      _/    _/  _/    _/  _/    _/_/  _/    _/      ",
+        "   _/      _/    _/    _/_/    _/      _/    _/_/_/       ",
+        "                                                          ",
+        "                                                          ",
       }
 
       dashboard.section.buttons.val = {
@@ -23,6 +23,15 @@ return {
         dashboard.button("SPC ee", "  > Toggle file explorer", ":NvimTreeToggle <CR>"),
         dashboard.button("SPC ff", "  > Find file", ":Telescope find_files <CR>"),
         dashboard.button("SPC fs", "  > Find text", ":Telescope live_grep <CR>"),
+      }
+
+      dashboard.config.layout = {
+        { type = "padding", val = 10 },
+        dashboard.section.header,
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        { type = "padding", val = 2 },
+        dashboard.section.footer,
       }
 
       alpha.setup(dashboard.opts)
