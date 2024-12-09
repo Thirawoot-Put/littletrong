f912416b888b690660f584688af4c9b33743f8b6
Thirawoot-Put <thirawoot.put@gmail.com>
2024-12-09 14:33:07 +0700

    delete codium

--- lua/littletrong/plugins/codeium.lua
+++
@@ -1,8 +0,0 @@
-return {
---"Exafunction/codeium.vim",
---config = function()
---	vim.keymap.set("i", "<c-x>", function()
---		return vim.fn["codeium#Clear"]()
---	end, { expr = true, silent = true })
---end,
-}
