a95a21dc080ecacfd5ff26bf0786424391336b06
Thirawoot.P <thirawoot.put@gmail.com>
2024-12-10 08:57:51 +0700

    describe get format

--- lua/littletrong/plugins/none-ls.lua
+++ lua/littletrong/plugins/none-ls.lua
@@ -38,6 +38,6 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Get format" })
   end,
 }
