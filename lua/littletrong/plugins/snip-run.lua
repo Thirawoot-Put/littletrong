7d7e5f5b1c003d467dd885fd33a6ad126302c1f7
Thirawoot.P <thirawoot.put@gmail.com>
2024-12-19 12:49:57 +0700

    dressing and SnipRun keymap

---
+++ lua/littletrong/plugins/snip-run.lua
@@ -0,0 +1,18 @@
+return {
+  "michaelb/sniprun",
+  branch = "master",
+
+  build = "sh install.sh",
+  -- do 'sh install.sh 1' if you want to force compile locally
+  -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+
+  config = function()
+    require("sniprun").setup({
+      -- your options
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>xf", ":SnipRun<CR>", { desc = "Execute file with SnipRun" })
+  end,
+}
