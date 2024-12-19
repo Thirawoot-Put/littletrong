3c1024f52be79745a5056b5b1bc89252a569bea8
Thirawoot.P <thirawoot.put@gmail.com>
2024-12-19 15:04:05 +0700

    new lsp config with blink

--- lua/littletrong/plugins/completions.lua
+++ lua/littletrong/plugins/completions.lua
@@ -1,101 +1,43 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-path",             -- source for file system paths
-    "L3MON4D3/LuaSnip",             -- snippet engine
-    "saadparwaiz1/cmp_luasnip",     -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "VonHeikemen/lsp-zero.nvim",
+  {
+    'saghen/blink.cmp',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+    },
+
+    version = 'v0.*',
+
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- see the "default configuration" section below for full documentation on how to define
+      -- your own keymap.
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer'},
+        -- optionally disable cmdline completions
+        -- cmdline = {},
+      },
+
+      -- experimental signature help support
+      -- signature = { enabled = true }
+    },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" }
   },
-  config = function()
-    local cmp = require("cmp")
-    local cmp_format = require("lsp-zero").cmp_format({ details = true })
-    local cmp_action = require("lsp-zero").cmp_action()
-    local luasnip = require("luasnip")
-
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          if not luasnip then
-            return
-          end
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        -- ["<S-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        -- ["<S-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp_action.luasnip_jump_forward(),
-        ["<C-f>"] = cmp_action.luasnip_jump_backward(),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp_action.luasnip_supertab(),
-        ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" },  -- text within current buffer
-        { name = "path" },    -- file system paths
-      }),
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = cmp_format,
-    })
-
-    -- `:` cmdline setup.
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
-
-    -- ******* use this for separate normal sql file and dadbod ********
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "sql", "mysql", "plsql" },
-      callback = function()
-        local is_dadbod_session = vim.fn.exists(":DB") == 2 -- Detect if `vim-dadbod` is in use
-        if is_dadbod_session then
-          cmp.setup.buffer({
-            sources = {
-              { name = "vim-dadbod-completion" }, -- Use Dadbod completion only in DB sessions
-            },
-          })
-        else
-          -- Normal SQL completion for non-Dadbod files
-          cmp.setup.buffer({
-            sources = cmp.config.sources({
-              { name = "buffer" },
-              { name = "path" },
-              { name = "nvim_lsp" }, -- Use LSP if available
-            }),
-          })
-        end
-      end,
-    })
-  end,
 }
--- lua/littletrong/plugins/lsp-config.lua
+++ lua/littletrong/plugins/lsp-config.lua
@@ -1,57 +1,74 @@
 return {
+  -- LSP servers and clients communicate which features they support through  capabilities".
+  --  By default, Neovim supports a subset of the LSP specification.
+  --  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+  --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+  --
+  -- This can vary by config, but in general for nvim-lspconfig:
+
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    lazy = true,
-    config = false,
-    init = function()
-      -- Disable automatic setup, we are doing it manually
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    lazy = true,
-    config = true,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    cmd = "LspInfo",
-    event = { "BufReadPre", "BufNewFile" },
+    'neovim/nvim-lspconfig',
     dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "williamboman/mason-lspconfig.nvim" },
+      'saghen/blink.cmp',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim'
     },
-    config = function()
-      -- This is where all the LSP shenanigans will live
-      local lsp_zero = require("lsp-zero")
-      local mason_lspconfig = require("mason-lspconfig")
-      local lspconfig = require("lspconfig")
-      lsp_zero.extend_lspconfig()
 
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        local opts = { buffer = bufnr }
+    -- example using `opts` for defining servers
+    opts = {
+      servers = {
+        lua_ls = {},
+        -- golang
+        gopls = {},
+        golangci_lint_ls = {},
 
-        lsp_zero.default_keymaps(opts)
-        vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<cr>")
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gx", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-        vim.keymap.set("n", "gj", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
-        vim.keymap.set("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+        -- typescript/javascript
+        ts_ls = {},
+        vtsls = {},
+        biome = {},
+        quick_lint_js = {},
 
-        lsp_zero.buffer_autoformat()
-      end)
+        -- frontend core
+        html = {},
+        cssls = {},
+        cssmodules_ls = {},
+        unocss = {},
+        tailwindcss = {},
 
-      -- (Optional) Configure lua language server for neovim
-      lsp_zero.set_sign_icons({
-        error = "✘",
-        warn = "▲",
-        hint = "⚑",
-        info = "»",
-      })
+        -- docker
+        dockerls = {},
+        docker_compose_language_service = {},
+
+        eslint = {},
+
+        -- haskell
+        --  hls = {},
+
+        -- openapi/yaml
+        spectral = {},
+        vacuum = {},
+        hydra_lsp = {},
+        yamlls = {},
+
+        prismals = {}, -- prisma
+
+        -- sql
+        sqlls = {},
+        sqls = {},
+
+        -- rust
+        rust_analyzer = {},
+
+        -- php
+        intelephense = {},
+      }
+    },
+
+    config = function(_, opts)
+      require('mason').setup()
+
+      local lspconfig = require('lspconfig')
+      local mason_lspconfig = require('mason-lspconfig')
 
       lspconfig.ts_ls.setup({
         root_dir = lspconfig.util.root_pattern(
@@ -61,15 +78,11 @@ return {
           ".eslintrc.yaml",
           ".eslintrc.yml",
           ".eslintrc.json"
-        -- Disabled to prevent "No ESLint configuration found" exceptions
-        -- 'package.json',
         ),
       })
-
       lspconfig.golangci_lint_ls.setup({
         filetypes = { "go", "gomod" },
       })
-
       lspconfig.sqls.setup({
         on_attach = function(client, _)
           client.server_capabilities.documentFormattingProvider = false
@@ -78,60 +91,16 @@ return {
       })
 
       mason_lspconfig.setup({
-        ensure_installed = {
-          -- golang
-          "gopls",
-          "golangci_lint_ls",
-
-          -- typescript/javascript
-          "ts_ls",
-          "vtsls",
-          "biome",
-          "quick_lint_js",
-
-          -- frontend core
-          "html",
-          "cssls",
-          "cssmodules_ls",
-          "unocss",
-          "tailwindcss",
-
-          -- docker
-          "dockerls",
-          "docker_compose_language_service",
-
-          "eslint",
-
-          -- haskell
-          -- "hls",
-
-          -- openapi/yaml
-          "spectral",
-          "vacuum",
-          "hydra_lsp",
-          "yamlls",
-
-          "prismals", -- prisma
-
-          -- sql
-          "sqlls",
-          "sqls",
-
-          -- rust
-          "rust_analyzer",
-
-          -- php
-          "intelephense",
-        },
+        ensure_installed = vim.tbl_keys(opts.servers), -- Automatically install listed servers
         automatic_installation = true,
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            lspconfig.lua_ls.setup(lua_opts)
-          end,
-        },
       })
-    end,
-  },
+
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end
+  }
 }
