ac8294ddf6f453cc2fba0ee90ec1ba1dae6b67e7
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-08 11:43:59 +0700

    use catpuccin

--- lua/littletrong/plugins/colorscheme.lua
+++ lua/littletrong/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
--- return {
---   "catppuccin/nvim",
---   lazy = false,
---   name = "catppuccin",
---   priority = 1000,
---   config = function()
---     require('catppuccin').setup({
--- integrations = { blink_cmp = true },
---     })
---     vim.cmd.colorscheme "catppuccin"
---   end
--- }
 return {
-  "folke/tokyonight.nvim",
-  lazy = false, -- Load immediately
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      style = "night"
+    require('catppuccin').setup({
+      integrations = { blink_cmp = true },
     })
-
-    vim.cmd("colorscheme tokyonight") -- Apply the colorscheme
-  end,
+    vim.cmd.colorscheme "catppuccin"
+  end
 }
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false, -- Load immediately
+--   config = function()
+--     require("tokyonight").setup({
+--       style = "moon"
+--     })
+--
+--     vim.cmd("colorscheme tokyonight") -- Apply the colorscheme
+--   end,
+-- }
