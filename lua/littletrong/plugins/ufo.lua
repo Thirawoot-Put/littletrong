d174b3415e3f7d5eb5ac201d0a07d3a25ee6f502
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-29 10:33:53 +0700

    refactor

--- lua/littletrong/plugins/ufo.lua
+++ lua/littletrong/plugins/ufo.lua
@@ -2,18 +2,16 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" }, -- Required dependency
 
-
   config = function()
     -- Configuration for nvim-ufo goes here
-    local ufo = require("ufo")
-    ufo.setup({
+    require("ufo").setup({
       provider_selector = function(bufnr, filetype, buftype)
         return { 'treesitter', 'indent' }
       end
     })
 
     vim.o.foldcolumn = '1' -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
 
