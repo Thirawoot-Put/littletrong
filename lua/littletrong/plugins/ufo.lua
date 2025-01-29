1c9a1914c20beb173a2638f14d75b3ea4c2a15b2
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-29 10:32:21 +0700

    use minimal config for ufo

--- lua/littletrong/plugins/ufo.lua
+++ lua/littletrong/plugins/ufo.lua
@@ -1,63 +1,24 @@
 return {
   "kevinhwang91/nvim-ufo",
   dependencies = { "kevinhwang91/promise-async" }, -- Required dependency
+
+
   config = function()
     -- Configuration for nvim-ufo goes here
     local ufo = require("ufo")
     ufo.setup({
-      -- Enable folding for all file types
-      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-        local newVirtText = {}
-        local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-        local sufWidth = vim.fn.strdisplaywidth(suffix)
-        local targetWidth = width - sufWidth
-        local curWidth = 0
-        for _, chunk in ipairs(virtText) do
-          local chunkText = chunk[1]
-          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-          else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, { chunkText, hlGroup })
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            if curWidth + chunkWidth < targetWidth then
-              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-          end
-          curWidth = curWidth + chunkWidth
-        end
-        table.insert(newVirtText, { suffix, "MoreMsg" })
-        return newVirtText
-      end,
       provider_selector = function(bufnr, filetype, buftype)
-        return { "treesitter", "indent" } -- Use treesitter and indent-based folding
-      end,
-      close_fold_kinds = { "imports" },   -- Automatically close folds for imports
-      preview = {
-        win_config = {
-          border = "rounded",             -- Border style for the preview window
-          winhighlight = "Normal:Folded", -- Highlight for the preview window
-          winblend = 0,                   -- Transparency for the preview window
-        },
-        mappings = {
-          scrollU = "<C-u>", -- Scroll up in the preview window
-          scrollD = "<C-d>", -- Scroll down in the preview window
-        },
-      },
+        return { 'treesitter', 'indent' }
+      end
     })
 
-    local keymap = vim.keymap
-    keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
-    keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
-    keymap.set("n", "zr", ufo.openFoldsExceptKinds, { desc = "Open folds except kinds" })
-    keymap.set("n", "zm", ufo.closeFoldsWith, { desc = "Close folds with" })
-    keymap.set("n", "zp", ufo.peekFoldedLinesUnderCursor, { desc = "Peek folded lines" })
+    vim.o.foldcolumn = '1' -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
 
-    local api = vim.api
-    api.nvim_set_hl(0, "Folded", { fg = "#c678dd", bg = "#282c34", italic = true })
-    api.nvim_set_hl(0, "FoldColumn", { fg = "#c678dd", bg = "#282c34" })
+    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
   end,
 }
