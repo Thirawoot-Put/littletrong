1e8395aec54e08732f343eeff47f6fb378acbe01
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-29 09:01:41 +0700

    without snack

--- lua/littletrong/plugins/alpha.lua
+++ lua/littletrong/plugins/alpha.lua
@@ -1,51 +1,51 @@
 return {
-  -- {
-  --   'goolord/alpha-nvim',
-  --   event = 'VimEnter',
-  --   config = function()
-  --     local alpha = require("alpha")
-  --     local dashboard = require("alpha.themes.dashboard")
-  --     local ctp = require("catppuccin.palettes").get_palette() -- Get the Catppuccin color palette
-  --
-  --     vim.api.nvim_set_hl(0, "DashboardHeader", { fg = ctp.lavender })
-  --
-  --     dashboard.section.header.val = {
-  --       [[                                                                                         ]],
-  --       [[     __     ______  ______  ______  __       ____               ______   __  __          ]],
-  --       [[    /\ \   /\__  _\/\__  _\/\__  _\/\ \     /\  _`\    /'\_/`\ /\  _  \ /\ \/\ \         ]],
-  --       [[    \ \ \  \/_/\ \/\/_/\ \/\/_/\ \/\ \ \    \ \ \L\_\ /\      \\ \ \L\ \\ \ `\\ \        ]],
-  --       [[     \ \ \  __\ \ \   \ \ \   \ \ \ \ \ \  __\ \  _\L \ \ \__\ \\ \  __ \\ \ , ` \       ]],
-  --       [[      \ \ \L\ \\_\ \__ \ \ \   \ \ \ \ \ \L\ \\ \ \L\ \\ \ \_/\ \\ \ \/\ \\ \ \`\ \      ]],
-  --       [[       \ \____//\_____\ \ \_\   \ \_\ \ \____/ \ \____/ \ \_\\ \_\\ \_\ \_\\ \_\ \_\     ]],
-  --       [[        \/___/ \/_____/  \/_/    \/_/  \/___/   \/___/   \/_/ \/_/ \/_/\/_/ \/_/\/_/     ]],
-  --       [[                                                                                         ]],
-  --       [[                                                                                         ]],
-  --     }
-  --
-  --     dashboard.section.header.opts = {
-  --       position = "center",
-  --       hl = "DashboardHeader"
-  --     }
-  --
-  --     dashboard.section.buttons.val = {
-  --       dashboard.button("e", "  > New file", "<cmd>ene <CR>"),
-  --       dashboard.button("SPC ee", "  > Toggle file explorer", ":NvimTreeToggle <CR>"),
-  --       dashboard.button("SPC ff", "  > Find file", ":Telescope find_files <CR>"),
-  --       dashboard.button("SPC fs", "  > Find text", ":Telescope live_grep <CR>"),
-  --     }
-  --
-  --     dashboard.config.layout = {
-  --       { type = "padding", val = 10 },
-  --       dashboard.section.header,
-  --       { type = "padding", val = 2 },
-  --       dashboard.section.buttons,
-  --       { type = "padding", val = 2 },
-  --       dashboard.section.footer,
-  --     }
-  --
-  --     alpha.setup(dashboard.opts)
-  --
-  --     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  --   end
-  -- },
+  {
+    'goolord/alpha-nvim',
+    event = 'VimEnter',
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+      -- local ctp = require("catppuccin.palettes").get_palette() -- Get the Catppuccin color palette
+
+      -- vim.api.nvim_set_hl(0, "DashboardHeader", { fg = ctp.lavender })
+
+      dashboard.section.header.val = {
+        [[                                                                                         ]],
+        [[     __     ______  ______  ______  __       ____               ______   __  __          ]],
+        [[    /\ \   /\__  _\/\__  _\/\__  _\/\ \     /\  _`\    /'\_/`\ /\  _  \ /\ \/\ \         ]],
+        [[    \ \ \  \/_/\ \/\/_/\ \/\/_/\ \/\ \ \    \ \ \L\_\ /\      \\ \ \L\ \\ \ `\\ \        ]],
+        [[     \ \ \  __\ \ \   \ \ \   \ \ \ \ \ \  __\ \  _\L \ \ \__\ \\ \  __ \\ \ , ` \       ]],
+        [[      \ \ \L\ \\_\ \__ \ \ \   \ \ \ \ \ \L\ \\ \ \L\ \\ \ \_/\ \\ \ \/\ \\ \ \`\ \      ]],
+        [[       \ \____//\_____\ \ \_\   \ \_\ \ \____/ \ \____/ \ \_\\ \_\\ \_\ \_\\ \_\ \_\     ]],
+        [[        \/___/ \/_____/  \/_/    \/_/  \/___/   \/___/   \/_/ \/_/ \/_/\/_/ \/_/\/_/     ]],
+        [[                                                                                         ]],
+        [[                                                                                         ]],
+      }
+
+      dashboard.section.header.opts = {
+        position = "center",
+        hl = "DashboardHeader"
+      }
+
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  > New file", "<cmd>ene <CR>"),
+        dashboard.button("SPC ee", "  > Toggle file explorer", ":NvimTreeToggle <CR>"),
+        dashboard.button("SPC ff", "  > Find file", ":Telescope find_files <CR>"),
+        dashboard.button("SPC fs", "  > Find text", ":Telescope live_grep <CR>"),
+      }
+
+      dashboard.config.layout = {
+        { type = "padding", val = 10 },
+        dashboard.section.header,
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        { type = "padding", val = 2 },
+        dashboard.section.footer,
+      }
+
+      alpha.setup(dashboard.opts)
+
+      vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end
+  },
 }
