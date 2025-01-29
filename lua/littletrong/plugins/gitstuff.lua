bca5a2624fccef59b3ee1f632c10f68681428c37
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-29 17:07:45 +0700

    delete gh.nvim

--- lua/littletrong/plugins/gitstuff.lua
+++ lua/littletrong/plugins/gitstuff.lua
@@ -10,18 +10,4 @@ return {
     "tpope/vim-fugitive",
     event = "VeryLazy", -- Load lazily
   },
-  {
-    "ldelossa/gh.nvim",
-    dependencies = {
-      {
-        "ldelossa/litee.nvim",
-        config = function()
-          require("litee.lib").setup()
-        end,
-      },
-    },
-    config = function()
-      require("litee.gh").setup()
-    end,
-  }
 }
