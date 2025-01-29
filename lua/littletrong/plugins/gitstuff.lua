483329ff1c85bd8e606c5414fc5b7af35cafa077
Thirawoot.P <thirawoot.put@gmail.com>
2025-01-29 16:56:08 +0700

    gh.nvim

--- lua/littletrong/plugins/gitstuff.lua
+++ lua/littletrong/plugins/gitstuff.lua
@@ -10,4 +10,18 @@ return {
     "tpope/vim-fugitive",
     event = "VeryLazy", -- Load lazily
   },
+  {
+    "ldelossa/gh.nvim",
+    dependencies = {
+      {
+        "ldelossa/litee.nvim",
+        config = function()
+          require("litee.lib").setup()
+        end,
+      },
+    },
+    config = function()
+      require("litee.gh").setup()
+    end,
+  }
 }
