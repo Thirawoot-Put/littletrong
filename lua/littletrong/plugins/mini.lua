4b573bdf346fda980031c1cd1431ebeb5cb1a13e
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-20 14:22:59 +0700

    refactor some code

--- lua/littletrong/plugins/mini.lua
+++ lua/littletrong/plugins/mini.lua
@@ -1,13 +1,15 @@
 return {
-  "echasnovski/mini.files",
-  config = function()
-    require('mini.files').setup()
+  {
+    "echasnovski/mini.files",
+    config = function()
+      require('mini.files').setup()
 
-    local keymap = vim.keymap
+      local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ep", function()
-      require("mini.files").open(vim.api.nvim_buf_get_name(0)) -- Open in the current file's directory
-    end, { desc = "Toggle Mini Files" })
-    keymap.set("n", "<leader>em", ":lua MiniFiles.open()<CR>")
-  end
+      keymap.set("n", "<leader>ep", function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0)) -- Open in the current file's directory
+      end, { desc = "Toggle Mini Files" })
+      keymap.set("n", "<leader>em", ":lua MiniFiles.open()<CR>")
+    end
+  }
 }
