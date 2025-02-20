55b8f6bb920f6be62c0eddda4656336e6e414f93
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-20 15:57:29 +0700

    yaml-companion

--- lua/littletrong/plugins/containerized.lua
+++ lua/littletrong/plugins/containerized.lua
@@ -2,5 +2,21 @@ return {
   {
     "ekalinin/Dockerfile.vim",
     ft = { "dockerfile", "Dockerfile" }
+  },
+  {
+    "andrewstuart/vim-kubernetes",
+    config = function()
+    end
+  },
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("yaml-companion").setup({
+        schemas = {
+          kubernetes = "https://json.schemastore.org/kubernetes.json",
+        },
+      })
+    end,
   }
 }
