834f2eb1caf58d39ac4d8a2492f6710c06e3cd72
Thirawoot.P <thirawoot.put@gmail.com>
2025-02-20 15:43:58 +0700

    diffview

--- lua/littletrong/plugins/gitstuff.lua
+++ lua/littletrong/plugins/gitstuff.lua
@@ -11,4 +11,15 @@ return {
     dependencies = { "tpope/vim-rhubarb" },
     event = "VeryLazy", -- Load lazily
   },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("diffview").setup({})
+
+      vim.keymap.set("n", "<leader>do", ":DiffviewOpen<CR>", { silent = true, desc = "Open DiffView" })
+      vim.keymap.set("n", "<leader>dx", ":DiffviewClose<CR>", { silent = true, desc = "Close DiffView" })
+      vim.keymap.set("n", "<leader>dh", ":DiffviewFileHistory<CR>", { silent = true, desc = "File History" })
+    end
+  },
 }
--- lua/littletrong/plugins/term.lua
+++ lua/littletrong/plugins/term.lua
@@ -10,7 +10,7 @@ return {
             if term.direction == "horizontal" then
               return 20
             elseif term.direction == "vertical" then
-              return vim.o.columns * 0.4
+              return vim.o.columns * 0.3
             end
           end,
         })
